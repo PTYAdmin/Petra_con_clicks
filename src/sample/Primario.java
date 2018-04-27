eb51c3ca9d7ad2d90a55dec2fd51d304931fc9a9
Jahiro <alex91085@hotmail.com>
2018-04-26 21:37:10 -0500

    Petra_ahi_va

---
+++ src/sample/Primario.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.scene.input.MouseEvent;
+
+
+public class Primario {
+    public void daleclick(MouseEvent mouseEvent) {
+        int conteo = 0;
+        conteo++;
+System.out.println(conteo);
+    }
+
+    public void area_fuera(MouseEvent mouseEvent) {
+    }
+
+    public void area_fuera_again(MouseEvent mouseEvent) {
+    }
+
+    public void fallaste_mira(MouseEvent mouseEvent) {
+
+    }
+}
